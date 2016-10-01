3435cfd0896e972649c7cc474facaab7b05ce824
Sascha Becker <s.becker@wertarbyte.com>
2016-10-01 18:20:14 +0200

    Add github login.

--- client/modules/core/components/ProfileMenu.jsx
+++ client/modules/core/components/ProfileMenu.jsx
@@ -1,67 +1,24 @@
 import React from 'react'
-import RaisedButton from 'material-ui/RaisedButton'
-import { LoginDialog } from './dialogs/LoginDialog'
-import { RegistrationDialog } from './dialogs/RegistrationDialog'
+import { RaisedButton } from 'material-ui'
 
-export class ProfileMenu extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loginOpen: false,
-      registerOpen: false
-    }
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  onLoginCancel() {
-    this.setState({ loginOpen: false })
-  }
-
-  onRegisterCancel() {
-    this.setState({ registerOpen: false })
-  }
-
-  registerOpen() {
-    this.setState({ registerOpen: true })
-  }
-
-  loginOpen() {
-    this.setState({ loginOpen: true })
+export default class ProfileMenu extends React.Component {
+  onLogin() {
+    console.log('login')
+    Meteor.loginWithGithub({
+      requestPermissions: ['user', 'public_repo']
+    }, (err) => {
+      if (err)
+        Session.set('errorMessage', err.reason || 'Unknown error')
+    })
   }
 
   render() {
     return (
-      <div>
-        <div>
-          <RaisedButton
-            label="Login"
-            style={{ margin: 12 }}
-            onMouseUp={() => this.loginOpen()}
-          />
-          <RaisedButton
-            label="Register"
-            style={{ margin: 12 }}
-            onMouseUp={() => this.registerOpen()}
-          />
-        </div>
-
-        <LoginDialog
-          open={this.state.loginOpen}
-          onCancel={() => this.onLoginCancel()}
-          onRequestClose={() => this.onLoginCancel()}
-        />
-
-        <RegistrationDialog
-          open={this.state.registerOpen}
-          onCancel={() => this.onRegisterCancel()}
-          onRequestClose={() => this.onRegisterCancel()}
-        />
-      </div>
+      <RaisedButton
+        label="Login"
+        style={{ margin: 12 }}
+        onTouchTap={() => this.onLogin()}
+      />
     )
   }
 }
